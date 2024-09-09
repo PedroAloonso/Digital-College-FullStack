5d9d89036c2469da07fa3aef29e483ace43b6b61
Pedro Alonso <96313569+PedroAloonso@users.noreply.github.com>
2024-09-09 15:07:22 +0000

    chore: função de pesquisa de id adicionada

--- Aula22/script.js
+++ Aula22/script.js
@@ -4,14 +4,48 @@ import { somarDoisValores } from "./util.js";
 btnSomar.onclick = (() => resultado.innerText = somarDoisValores())
 loadUsersBtn.onclick = (() => colocarUsers())
 
-const colocarUsers = () => fetchUsers()
-    .then(users => {
-        usersDiv.innerHTML = ''
-        console.log(users);
-        users.forEach(user => criarUserCard(user))})
-    .catch(error => console.error('Error:', error))
+const searchId = () => {
+    if (userId.value >= 1) {
+        fetchUsers()
+            .then(users => {
+                usersDiv.innerHTML = ''
+                console.log(users);
+                users.forEach(user => { 
+                    if (user.id == userId.value) {
+                        console.log('deu certo')
+                        criarUserCard(user)
+                    }
+                })})
+            .catch(error => console.error('Error:', error))
+    } else {
+
+    }
+        
+}
 
 
+const colocarUsers = () => {
+    if (userId.value >= 1) {
+        fetchUsers()
+            .then(users => {
+                usersDiv.innerHTML = ''
+                console.log(users);
+                users.forEach(user => { 
+                    if (user.id == userId.value) {
+                        criarUserCard(user)
+                    }
+                })})
+            .catch(error => console.error('Error:', error))
+    } else {
+        fetchUsers()
+            .then(users => {
+                usersDiv.innerHTML = ''
+                console.log(users);
+                users.forEach(user => criarUserCard(user))
+            })
+            .catch(error => console.error('Error:', error))
+    }
+}
 
 function criarUserCard(user) {
     usersDiv.innerHTML += `<div class= 'userCard'>
