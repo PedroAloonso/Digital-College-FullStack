7ae7ee115782e9b19fbcbf22e42b1a5012db10eb
PedroAloonso <pedro.alonsopa@hotmail.com>
2024-10-28 16:00:25 -0300

    chore: finish the carousel

--- Aula01/src/components/Carousel/Carousel.jsx
+++ Aula01/src/components/Carousel/Carousel.jsx
@@ -1,16 +1,35 @@
 import style from "./Carousel.module.css"
 import Snearks from '/src/assets/img/carousel/White-Sneakers-PNG-Clipart 1.png'
+import { useEffect } from "react";
 
-export default function Carousel() {
+export default function Carousel({ setActiveIndex, activeIndex, length }) {
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setActiveIndex((prevIndex) =>
+                prevIndex === length - 1 ? 0 : prevIndex + 1
+            );
+        }, 2000);
+
+        return () => clearInterval(interval);
+    }, [length, setActiveIndex]);
     return (
         <>
             <div className={style.carouselContainer}>
                 <img src={Snearks} alt="" className={style.carouselImage} />
                 <div className={style.carrouselDots}>
-                    <span className={`${style.dots} active`}></span>
+                    {[...Array(length)].map((_, index) => (
+                        <span
+                            key={index}
+                            className={`${style.dots} ${
+                                index === activeIndex ? style.active : ""
+                            }`}
+                            onClick={() => setActiveIndex(index)}
+                        ></span>
+                    ))}
+                    {/* <span className={`${style.dots}  ${style.active}`}></span>
                     <span className={style.dots}></span>
                     <span className={style.dots}></span>
-                    <span className={style.dots}></span>
+                    <span className={style.dots}></span> */}
                 </div>
             </div>
         </>
--- Aula01/src/containers/Home/Home.jsx
+++ Aula01/src/containers/Home/Home.jsx
@@ -1,8 +1,12 @@
 import style from "./home.module.css";
 import Carousel from "../../components/Carousel/Carousel";
 import Ornament from "/src/assets/img/carousel/ornament1.png";
+import { useState } from "react";
 
 export default function Home() {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [length, setlength] = useState(4);
+
     return (
         <>
             <section className={style.firstSection}>
@@ -17,7 +21,11 @@ export default function Home() {
                         Ver Ofertas
                     </button>
                 </div>
-                <Carousel></Carousel>
+                <Carousel
+                    setActiveIndex={setActiveIndex}
+                    length={length}
+                    activeIndex={activeIndex}
+                />
                 <img src={Ornament} className={style.firstSectionOrnament} />
             </section>
         </>
