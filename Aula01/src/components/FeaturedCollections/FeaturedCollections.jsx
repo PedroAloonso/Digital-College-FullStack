1339940d17d3e13618772c5ecbc03db2afd065d3
PedroAloonso <pedro.alonsopa@hotmail.com>
2024-11-14 20:55:25 -0300

    chore: finish featured collections top side

--- Aula01/src/components/FeaturedCollections/FeaturedCollections.jsx
+++ Aula01/src/components/FeaturedCollections/FeaturedCollections.jsx
@@ -1,8 +1,8 @@
 import style from "./FeaturedCollections.module.css";
 import FeaturedCollectionsCard from "../FeaturedCollectionsBanner/FeaturedCollectionsCard";
-import t_shirt from "/src/assets/img/featuredCollections/t-shirt.png"
-import headphone from "/src/assets/img/featuredCollections/headphone.png"
-import tenis from "/src/assets/img/featuredCollections/tenis.png"
+import t_shirt from "/src/assets/img/featuredCollections/t-shirt.png";
+import headphone from "/src/assets/img/featuredCollections/headphone.png";
+import tenis from "/src/assets/img/featuredCollections/tenis.png";
 
 export default function FeaturedCollections() {
     return (
@@ -10,9 +10,51 @@ export default function FeaturedCollections() {
             <div className={style.featuredCollectionsTop}>
                 <h1>Coleções em destaque</h1>
                 <div>
-                    <FeaturedCollectionsCard title={"Novo drop Supreme"} image={t_shirt} />
-                    <FeaturedCollectionsCard title={"Coleção Adidas"} image={headphone} />
-                        <FeaturedCollectionsCard title={"Novo Beats Bass"} image={tenis} />
+                    <FeaturedCollectionsCard
+                        title={"Novo drop Supreme"}
+                        image={t_shirt}
+                        imagePosition={{
+                            top: "30%",
+                            right: "none",
+                            bottom: "none",
+                            left: "none",
+                        }}
+                        imageSize={{
+                            width: "auto",
+                            height: "236px",
+                        }}
+                        imageRotation={"-20deg"}
+                    />
+                    <FeaturedCollectionsCard
+                        title={"Novo Beats Bass"}
+                        image={tenis}
+                        imagePosition={{
+                            top: "20%",
+                            right: "-45%",
+                            botton: "none",
+                            left: "none",
+                        }}
+                        imageSize={{
+                            width: "263px",
+                            height: "200px",
+                        }}
+                        imageRotation={`-10deg`}
+                    />
+                    <FeaturedCollectionsCard
+                        title={"Coleção Adidas"}
+                        image={headphone}
+                        imagePosition={{
+                            top: "none",
+                            right: "none",
+                            bottom: "-10%",
+                            left: "none",
+                        }}
+                        imageSize={{
+                            width: "auto",
+                            height: "250px",
+                        }}
+                        imageRotation={"30deg"}
+                    />
                 </div>
             </div>
             <div className={style.featuredCollectionsBotton}></div>
--- Aula01/src/components/FeaturedCollectionsBanner/FeaturedCollectionsCard.jsx
+++ Aula01/src/components/FeaturedCollectionsBanner/FeaturedCollectionsCard.jsx
@@ -1,16 +1,34 @@
 import style from "./FeaturedCollectionsCard.module.css";
 
-export default function FeaturedCollectionsCard({ title, image}) {
-  return (
-    <div className={style.container}>
-      <div className={style.infoContainer}>
-        <p className={style.discount}>30 % OFF</p>
-        <h2 className={style.title}>{title}</h2>
-        <button className={style.buyBtn}>Comprar</button>
-      </div>
-      <div className={style.imgContainer}>
-        <img src={image} alt="" />
-      </div>
-    </div>
-  );
+export default function FeaturedCollectionsCard({
+    title,
+    image,
+    imagePosition,
+    imageSize,
+    imageRotation,
+}) {
+    const customStyle = {
+        width: imageSize.width,
+        height: imageSize.height,
+
+        position: 'absolute',
+        top: imagePosition.top,
+        right: imagePosition.right,
+        bottom: imagePosition.bottom,
+        left: imagePosition.left,
+
+        transform: `rotate(${imageRotation})`,
+    };
+    return (
+        <div className={style.container}>
+            <div className={style.infoContainer}>
+                <p className={style.discount}>30 % OFF</p>
+                <h2 className={style.title}>{title}</h2>
+                <button className={style.buyBtn}>Comprar</button>
+            </div>
+            <div className={style.imgContainer}>
+                <img src={image} style={customStyle} alt="" />
+            </div>
+        </div>
+    );
 }
