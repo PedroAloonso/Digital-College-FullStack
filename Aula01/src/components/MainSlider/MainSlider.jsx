91f562cd19171b22e4b8db837cc4125639ee682d
PedroAloonso <pedro.alonsopa@hotmail.com>
2024-11-18 11:48:26 -0300

    refactor: change carousel to swiper

---
+++ Aula01/src/components/MainSlider/MainSlider.jsx
@@ -0,0 +1,68 @@
+import style from "./mainSlider.module.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import PrimaryButton from "../Buttons/PrimaryButton";
+import Ornament from "/src/assets/img/slider/ornament1.png";
+import Tennis from "/src/assets/img/slider/White-Sneakers.png";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+export default function MainSlider() {
+    const sliderContent = [
+        {
+            subtitle: "Melhores ofertas personalizadas",
+            title: "Queima de stoque Nike 🔥",
+            about: "Consequat conta exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur.",
+            image: Tennis,
+        },
+        {
+            subtitle: "Melhores ofertas que você ja viu ",
+            title: "Queima de stoque olympikus 🔥",
+            about: "Consequat até exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur.",
+            image: Tennis,
+        },
+        {
+            subtitle: "Melhores ofertas personalizadas",
+            title: "Queima de stoque Adidas ",
+            about: "Consequat o pegar exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur.",
+            image: Tennis,
+        },
+        {
+            subtitle: "Melhores ofertas personalizadas",
+            title: "Queima de stoque Lacoste 🐊",
+            about: "Consequat dez exercitation mollit nisi excepteur do do tempor laboris eiusmod irure consectetur.",
+            image: Tennis,
+        },
+    ];
+    return (
+        <section className={style.externalContainer}>
+            <Swiper
+                pagination={true}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                {sliderContent.map((value, index) => {
+                    return (
+                        <SwiperSlide key={index}>
+                            <div className={style.container}>
+                                <div className={style.info}>
+                                    <h3>{value.subtitle}</h3>
+                                    <h1>{value.title}</h1>
+                                    <p>{value.about}</p>
+                                    <PrimaryButton className={style.offersBtn}>
+                                        Ver Ofertas
+                                    </PrimaryButton>
+                                </div>
+                                <div className={style.imageContainer}>
+                                    <img src={value.image} alt="" />
+                                </div>
+                            </div>
+                            <img src={Ornament} className={style.ornament} />
+                        </SwiperSlide>
+                    );
+                })}
+            </Swiper>
+        </section>
+    );
+}
--- Aula01/src/containers/Home/Home.jsx
+++ Aula01/src/containers/Home/Home.jsx
@@ -1,14 +1,14 @@
 import style from "./home.module.css";
-import FirstBanner from "../../components/FirstBanner/FirstBanner";
+import MainSlider from "../../components/MainSlider/MainSlider";
 import FeaturedCollections from "../../components/FeaturedCollections/FeaturedCollections";
 import TredingProducts from "../../components/TredingProducts/TredingProducts";
 
 export default function Home() {
     return (
         <>
-            <FirstBanner />
+            <MainSlider />
             <FeaturedCollections />
-            <TredingProducts/>
+            <TredingProducts />
         </>
     );
 }
