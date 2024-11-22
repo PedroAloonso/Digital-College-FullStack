3cc789f9eb88efd20f7fb5203c26f90ac7501964
PedroAloonso <pedro.alonsopa@hotmail.com>
2024-11-22 18:21:11 -0300

    chore: sketch products container

--- Aula01/src/containers/Home/Home.jsx
+++ Aula01/src/containers/Home/Home.jsx
@@ -1,4 +1,3 @@
-import style from "./home.module.css";
 import MainSlider from "../../components/MainSlider/MainSlider";
 import FeaturedCollections from "../../components/FeaturedCollections/FeaturedCollections";
 import TredingProducts from "../../components/TredingProducts/TredingProducts";
--- Aula01/src/containers/Products/Products.jsx
+++ Aula01/src/containers/Products/Products.jsx
@@ -1,12 +1,51 @@
-import { Link } from "react-router-dom";
+import ProductCard from "../../components/TredingProducts/ProductCard/ProductCard";
+import style from "./products.module.css";
 
 export default function Products() {
     return (
+        // DEPOIS COMPONETIZAR
         <>
-            <h1>Products </h1>
-            <p>
-                <Link to={"/"}>Home</Link>
-            </p>
+            <section className={style.topSection}>
+                <p>Resultados para “...”</p>
+                <div>
+                    <p>Ordenar por...</p>
+                </div>
+            </section>
+            <section className={style.bottomSection}>
+                <div className={style.filterContainer}>
+                    <p>Filtrar por</p>
+                </div>
+                <div className={style.productContainer}>
+                    <ProductCard
+                        category={"Tênis"}
+                        title={"K-Swiss V8 - Masculino"}
+                        value={200}
+                        newValue={100}
+                        discount={30}
+                    />
+                    <ProductCard
+                        category={"Tênis"}
+                        title={"K-Swiss V8 - Masculino"}
+                        value={200}
+                        newValue={100}
+                        discount={30}
+                    />
+                    <ProductCard
+                        category={"Tênis"}
+                        title={"K-Swiss V8 - Masculino"}
+                        value={200}
+                        newValue={100}
+                        discount={30}
+                    />
+                    <ProductCard
+                        category={"Tênis"}
+                        title={"K-Swiss V8 - Masculino"}
+                        value={200}
+                        newValue={100}
+                        discount={30}
+                    />
+                </div>
+            </section>
         </>
     );
 }
