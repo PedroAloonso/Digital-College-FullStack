fc9ee1ec657b4744306229611969d770de2da7fb
PedroAloonso <pedroalonsopa@hotmail.com>
2024-12-09 22:09:56 -0300

    feat: add text variables files and modify colors file

--- Projeto/src/App.jsx
+++ Projeto/src/App.jsx
@@ -5,9 +5,9 @@ import Products from "./containers/Products/Products";
 import Categories from "./containers/Categories/Categories";
 import MyOrders from "./containers/MyOrders/MyOrders";
 import "./App.css";
-import "./assets/styles/variables.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./assets/styles/ColorsVariables.css";
 import { Route, Routes, Outlet } from "react-router-dom";
+import ProductPage from "./components/ProductPage/ProductPage";
 
 export default function App() {
     return (
@@ -24,8 +24,17 @@ export default function App() {
                     }
                 >
                     <Route index element={<Home />} />
-                    #TODO Estudar o react route para usar e criar um produto
-                    <Route path="products" element={<Products />} />
+                    {/* 1: Criar uma rota index dentro da rota products que vai para o componente
+                        Products, depois criar uma rota que recebe um valor de identificação 
+                        do produto na propriedade path :valor, que leva para um componente ProductPage,
+                        que recebe esse valor numa variavel por meio da função useParams() do react-route.
+                        2: testar usar o path="*" dentro da rota products para ver oque acontece
+                    */}
+                    <Route path="products" element={<Products />}>
+                        <Route path="products/:categorie/:mark/:name" element={<ProductPage />} />
+                        <Route path="products/*" element={<ProductPage />} />
+
+                    </Route>
                     <Route path="categories" element={<Categories />} />
                     <Route path="my-orders" element={<MyOrders />} />
                     <Route path="*" element={<Home />} />
