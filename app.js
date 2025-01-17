75926b02f523db6f735b883629e183d0052849ce
PedroAloonso <pedro.alonsopa@hotmail.com>
2025-01-17 00:14:25 -0300

    feat: restructure app.js and add index.html for user interaction

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from "express";
-import prismaUserRoutes from "./routes/prisma-user.js";
-import sequelizeUserRoutes from "./routes/sequelize-user.js";
+import cors from "cors"
+import prismaUserRoutes from "./src/routes/prisma-user.js";
+import sequelizeUserRoutes from "./src/routes/sequelize-user.js";
 
 const app = express();
+app.use(cors())
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("src"))
 
 app.use("/prisma-users", prismaUserRoutes);
 app.use("/sequelize-users", sequelizeUserRoutes);
