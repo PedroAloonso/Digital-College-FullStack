877f19bd6e3ccd8fd5a03b9b16d807a7ea9d2e99
PedroAloonso <pedro.alonsopa@hotmail.com>
2025-02-28 17:07:50 -0300

    feat: add axios dependency; refactor Dashboard component and update styles for ChartsContainer and HightlightsCard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,5 @@
 import style from "./dashboard.module.scss";
 
-import MoneyIcon from "../../assets/img/dashboard/money.svg?react";
 import CardsContainer from "../../components/dashboard/CardsContainer/CardsContainer";
 import ChartsContainer from "../../components/dashboard/ChartsContainer/ChartsContainer";
 
