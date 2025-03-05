dfd40d5f45cf127319716dabdc96fec0446cb9f5
PedroAloonso <pedro.alonsopa@hotmail.com>
2025-03-05 11:09:56 -0300

    feat: enhance product routes with detailed responses and add update functionality

--- src/routes/sequelize/sequelize-product.js
+++ src/routes/sequelize/sequelize-product.js
@@ -6,7 +6,6 @@ const router = Router();
 // Criar um novo produto
 router.post("/", async (req, res) => {
     const { name, brand, description, stock } = req.body;
-    // console.log(req, "corpo da requisição")
     try {
         const product = await Product.create({ name, brand, description, stock });
         res.status(200).json(product);
@@ -18,24 +17,38 @@ router.post("/", async (req, res) => {
 
 // Listar todos os produtos
 router.get("/", async (req, res) => {
+    const columnNames = Object.entries(Product.getAttributes()).map(([columnName, detais]) => ({
+        name: columnName,
+        type: detais.type.key
+    }))
+
     const product = await Product.findAll();
-    res.json(product);
+    res.status(200).json({columnNames,product});
 });
 
 // Buscar um produto por ID
 router.get("/:id", async (req, res) => {
     const { id } = req.params;
     const product = await Product.findByPk(id);
-    if (!product) return res.status(404).json({ error: "Produto não encontrado" });
-    res.json(product);
+    if (!product) return res.status(404).json({ error: "Produto não encontrado get" });
+    res.status(200).json(product);
 });
 
 // Deleta um produto
 router.delete("/:id", async (req, res) => {
     const { id } = req.params;
     const product = await Product.findByPk(id);
-    if (!product) return res.status(404).json({ error: "Produto não encontrado" });
+    if (!product) return res.status(404).json({ error: "Produto não encontrado delete" });
     await product.destroy();
-    res.json(product);
+    res.status(200).json(product);
+});
+
+router.put("/:id", async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByPk(id);
+    if (!product) return res.status(404).json({ error: "Produto não encontrado Put" });
+    product.set(req.body)
+    product.save()
+    res.status(200).json(product);
 });
 export default router;
